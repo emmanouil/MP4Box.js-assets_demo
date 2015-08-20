d02366d3f20777b2aa53c9ec3395318f0f4f1f60
emmanouil <potetsie@imag.fr>
2015-08-20 16:57:56 +0200

    added dynamic cues

--- montmarte/assets/script.js
+++ montmarte/assets/script.js
@@ -1,17 +1,24 @@
 var video = document.querySelector('video');
 var textTrack = video.textTracks[0];
 var test = document.getElementById('test');
+var elements = [];	//contains active (visible) cues
+var cueHTML;	//html to be in DOM
 
 //video.addEventListener("play", function() { console.log('two'); video.pause(); }, true);
+
+//we have a cuew change
 textTrack.oncuechange = function(){
 	var counter = 0;
 	var looper = true;
+	cueHTML = null;
+	
 	do{
 		var cue = this.activeCues[counter];
 		if(!!cue && (!cue.poped)){
 			cue.poped=true;
 			looper=false;
-			test.innerHTML = cue.startTime + '-' + cue.endTime + ': ' + cue.text + '<br />' + test.innerHTML;
+			elements.push(cue);
+			//test.innerHTML = 'counter: '+ counter+ ' time: '+ video.currentTime + ' ___ ' + cue.startTime + '-' + cue.endTime + ': ' + cue.text + '<br />' + test.innerHTML;
 			console.log(cue.text);
 		}else if(!cue){
 			looper=false;
@@ -20,5 +27,16 @@ textTrack.oncuechange = function(){
 		}
 	}while(looper);
 	
+	//find expired cues
+	elements.forEach(function (item, index, array){
+	console.log('item: '+ item.endTime + '  video: '+video.currentTime);
+		if(item.endTime < video.currentTime){
+			elements.splice(index, 1);
+		}else{
+			cueHTML+=item.text;
+		}
+	});
+	
+	test.innerHTML=cueHTML;
 };
 //video.play();
